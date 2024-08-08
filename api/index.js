7508dfe8f72ff9df98372bff0a83d7b033bdc104
Abhimanyu Kumar <abhimanyukumar2090@gmail.com>
2024-08-08 14:30:28 +0530

    compete update listing functionality

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ import path from 'path';
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGO)
+  .connect( "mongodb://127.0.0.1:27017/SocProject")
   .then(() => {
     console.log('Connected to MongoDB!');
   })
