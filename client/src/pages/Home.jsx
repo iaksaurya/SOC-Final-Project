fd43daea8dc7ad8ca44a0c7958adf551cff8f35d
Abhimanyu Kumar <abhimanyukumar2090@gmail.com>
2024-08-08 23:07:42 +0530

    update all remaining

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -54,8 +54,8 @@ export default function Home() {
           <br />
           place with ease
         </h1>
-        <div className='text-gray-400 text-xs sm:text-sm'>
-          Sahand Estate is the best place to find your next perfect place to
+        <div className='tesxt-gray-400 text-xs sm:text-sm'>
+          Saurya Estate is the best place to find your next perfect place to
           live.
           <br />
           We have a wide range of properties for you to choose from.
